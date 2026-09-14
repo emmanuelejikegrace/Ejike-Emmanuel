e3a512c7749f8778eccaf73bb42d4746afa4487d
stephen okopi <okopistephen1@gmail.com>
2026-09-14 16:13:21 +0100

    todo.py

--- todo.py
+++ todo.py
@@ -5,14 +5,22 @@ while True:
     print("1. Add new task")
     print("2. View tasks")
     print("3. Remove task")
-    print("4. Exit")
+    print("4. Edit task")
+    print("5. Exit!")
 
     choice = input("choose an option: ")
 
     if choice == "1":
         task = input("Enter a task: ")
-        tasks.append(task)
-        print("Task added!")
+
+        if task.strip() == "":
+            print("Task cannot be empty!")
+
+        elif task in tasks:
+            print("Task already exists!")    
+        else:
+            tasks.append(task)
+            print("Task added!")    
 
     elif choice == "2":
         if len(tasks) == 0:
@@ -44,9 +52,37 @@ while True:
             print("Please enter a valide number")             
 
     elif choice == "4":
-        print("Thank you for useing the To-Do list. Goodbye!")
+        if len(tasks) == 0:
+            print("you have no task to edit!")
+
+        else:
+            for number, task in enumerate(tasks, start=1):
+                print(f"{number}. {task}")
+
+            try:
+                task_number = int(input("Enter the number of the task you want to edit: "))
+
+                if task_number >= 1 and task_number <= len(tasks):
+                    new_task = input("Enter the new task: ")
+
+                    if new_task.strip() == "":
+                        print("Task cannot be empty!")
+                    else:
+                        tasks[task_number - 1] = new_task
+                        print("Task updated!")
+
+                else:
+                    print("Invalid task number")
+
+            except ValueError:
+                print("please enter a valid number")
+    elif choice == "5":
+        print("Thank you for using the to-do list, Goodbye!")
         break
     else:
-        print("Invalid choice. Choose a number between 1 to 4!")                   
+        print("Invalid choice, please choose a number between 1 to 5")
+
+
+
 
 
